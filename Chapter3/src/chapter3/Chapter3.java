9a79e20308504b79ceeedc5d23f220a497483433
Eric Charnesky <eric.charnesky@careevolution.com>
2021-05-24 19:42:51 -0400

    string equal ignore case and swtich

--- Chapter3/src/chapter3/Chapter3.java
+++ Chapter3/src/chapter3/Chapter3.java
@@ -110,77 +110,134 @@ public class Chapter3 {
         } else {
             System.out.println("Your nubmer is in the ones");
         }
-        
+
         // logical and
         // true && true == true
         // true && false == false
         // false && true == false
         // false && false == false
-        
         // logical or
         // true || true == true
         // true || false == true
         // false || true == true
         // false || false == false
-        
-        
         // NOT mutually exclusive - be careful with the if blocks
-        if ( someNumber >= 1_000_000_000 ){
+        if (someNumber >= 1_000_000_000) {
             System.out.println("Your number is in the billions");
         }
-        if ( someNumber < 1_000_000_000 && someNumber >= 100_000_000 ){
+        if (someNumber < 1_000_000_000 && someNumber >= 100_000_000) {
             System.out.println("Your number is in the hundred millions");
         }
-        if ( someNumber < 100_000_000 && someNumber >= 10_000_000 ){
+        if (someNumber < 100_000_000 && someNumber >= 10_000_000) {
             System.out.println("Your number is in the ten millions");
         }
-        if ( someNumber < 10_000_000 && someNumber >= 1_000_000 ){
+        if (someNumber < 10_000_000 && someNumber >= 1_000_000) {
             System.out.println("Your number is in the millions");
         }
-        if ( someNumber < 1_000_000 && someNumber >= 100_000 ){
+        if (someNumber < 1_000_000 && someNumber >= 100_000) {
             System.out.println("Your number is in the hundred thousands");
         }
-        if ( someNumber < 100_000 && someNumber >= 10_000 ){
+        if (someNumber < 100_000 && someNumber >= 10_000) {
             System.out.println("Your number is in the ten thousands");
         }
-        if ( someNumber < 10_000 && someNumber >= 1_000 ){
+        if (someNumber < 10_000 && someNumber >= 1_000) {
             System.out.println("Your number is in the thousands");
         }
-        if ( someNumber < 1_000 && someNumber >= 100 ){
+        if (someNumber < 1_000 && someNumber >= 100) {
             System.out.println("Your number is in the hundreds");
         }
-        if ( someNumber < 100 && someNumber >= 10 ){
+        if (someNumber < 100 && someNumber >= 10) {
             System.out.println("Your number is in the tens");
         } // you can't have a default because there are no else blocks
-        if ( someNumber < 10 && someNumber >= 1 ){
+        if (someNumber < 10 && someNumber >= 1) {
             System.out.println("Your number is in the tens");
         }
-        
+
         System.out.println("What's the temperature outside?");
         int temperature = Integer.parseInt(keyboard.nextLine());
         // farenhiet == sad
-        if ( temperature < 32 ){
+        if (temperature < 32) {
             System.out.println("Bring your winter coat");
-        }
-        else if ( temperature < 50){
+        } else if (temperature < 50) {
             System.out.println("Bring your jacket");
-        }
-        else{
+        } else {
             System.out.println("you're fine in a t-shirt");
         }
-        
+
         System.out.println("What's the weather like? (rain, snow, sun)");
         String weather = keyboard.nextLine();
-        
-        // don't use == for strings
-        if ( weather == "rain" ){
+
+        // don't use == for strings -- it doesn't work =(
+        if (weather == "rain") {
             System.out.println("bring an umbrella");
-        } else if ( weather == "snow"){
+        } else if (weather == "snow") {
             System.out.println("Bring your gloves");
-        } else{
+        } else {
             System.out.println("it's a nice day!");
         }
 
+        // when comparing strings, use equalsIgnoreCase or equals, whichever you need
+        if (weather.equalsIgnoreCase("rain")) {
+            System.out.println("bring your umbrella!");
+        } else if (weather.equalsIgnoreCase("snow")) {
+            System.out.println("bring your gloves!");
+        } else {
+            System.out.println("bring your suncreeen!");
+        }
+
+        if (weather.equalsIgnoreCase("rain")) {
+            // only exists inside the curly braces it was declared in
+            String item = "umbrella";
+        } else if (weather.equalsIgnoreCase("snow")) {
+            String item = "gloves";
+        } else {
+            String item = "sunscreen";
+        }
+        //System.out.println("bring your " + item + "!");
+
+        String item;
+        if (weather.equalsIgnoreCase("rain")) {
+            item = "umbrella";
+        } else if (weather.equalsIgnoreCase("snow")) {
+            item = "gloves";
+        } else {
+            item = "sunscreen";
+        }
+        System.out.println("bring your " + item + "!");
+
+        
+        String anotherItem;
+        // can't switch using equalsIgnoreCase
+        switch (weather.toLowerCase())
+        {
+            case "rain":
+                anotherItem = "umbrella";
+                break;
+            case "snow":
+                anotherItem = "gloves";
+                break;
+            default:
+                anotherItem = "sunscreen";
+                // no break needed, the switch is already done
+        }
+        
+        System.out.println("Enter how much money you have for lunch: ");
+        double lunchMoney = Double.parseDouble(keyboard.nextLine());
+
+        String lunchChoice;
+        if (lunchMoney >= 5.00) {
+            lunchChoice = "Burger King";
+        } else {
+            lunchChoice = "Ramen Noodles";
+        }
+        
+        System.out.println("lunch tody is: " + lunchChoice);
+
+        // ternary expression or condional operator
+        String otherLunchChoice = lunchMoney < 5.00 ? "Ramen Noodles" : "Burger King";
+        System.out.println("lunch tody is: " + otherLunchChoice);
+        
+        
     }
 
 }
